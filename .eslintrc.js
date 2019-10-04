4d9a9d7787f5e8708d084e6ca4a2be270207405e
Alex de Melo Silva <alexmsilva@gmail.com>
2019-10-04 00:50:51 -0300

    Configures project and environment

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    plugins: ['prettier'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-trailing-spaces': 'error',
+        'eol-last': ['error', 'always'],
+        'indent': ['error', 4],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 1 }],
+        'class-methods-use-this': 'off',
+        'no-param-reassign': 'off',
+        'camelcase': 'off',
+        'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }]
+    }
+};
