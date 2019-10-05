238909e34ea828904bab2ea644134e6e8e8b1ff2
Alex de Melo Silva <alexmsilva@gmail.com>
2019-10-05 12:07:34 -0300

    Configures Sequelize ORM to connect to Postgres

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'postgres',
+    username: 'postgres',
+    password: 'gobarber',
+    database: 'meetapp',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+};
