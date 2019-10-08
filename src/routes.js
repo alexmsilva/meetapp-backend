48a8b3b9660b234d3ce8d9efc7ae0abf73f9e0c6
Alex de Melo Silva <alexmsilva@gmail.com>
2019-10-07 21:58:55 -0300

    Adds file upload (banner) feature using Multer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => res.json({ message: 'Hello, Welcome to Meetapp!' }));
 
@@ -15,5 +20,7 @@ routes.post('/sessions', SessionController.create);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.create);
 
 export default routes;
